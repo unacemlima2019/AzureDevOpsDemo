b253f1c9677895215dfc56d277b58d2c18b8de34
Jhoel <jhoel.leon@tpaconsultores.com.pe>
2022-08-18 21:57:24 -0500

    Inicio Proyecto

---
+++ src/KodotiAzureDevOps.Api/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace KodotiAzureDevOps.Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class DefaultController : ControllerBase
+    {
+        private static DateTime? ReleaseDate { get; set; }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            if (ReleaseDate == null) 
+            {
+                ReleaseDate = DateTime.UtcNow;
+            }
+
+            return Ok(
+                new
+                {
+                    Enviroment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"),
+                    ReleaseDate
+                }
+            );
+        }
+    }
+}
